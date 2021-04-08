4ec52b172ee7379b684bc94c57e8f50b1a8f0441
Ke Yue <keeyue@gmail.com>
2021-04-08 17:19:37 -0500

    More document

--- src/IPtychoScanMesh.cpp
+++ src/IPtychoScanMesh.cpp
@@ -851,11 +851,11 @@ void IPtychoScanMesh::get_nonoverlapping_indices()
 			indices_0.push_back(j);
 		std::random_shuffle ( indices_0.begin(), indices_0.end() );
 
-		// Testing with matlab
-		char* filename="/data2/JunjingData/indices.csv";
-		std::vector<int> vecFile;
-		loadTestIGroup(filename, vecFile);
-		indices_0=vecFile;
+		// test with matlab
+//		char* filename="/data2/JunjingData/indices.csv";
+//		std::vector<int> vecFile;
+//		loadTestIGroup(filename, vecFile);
+//		indices_0=vecFile;
 
 		std::vector< std::vector<int> > indices (maxNpos, std::vector<int>() ); //indices_out
 
